ee0a69267938c94b8043e9522f1c12f39f7805ce
Samah Rajabi <samahra@edu.hac.ac.il>
2021-12-27 21:14:54 +0200

    in main: get width and height from background image (instead of using global consts)

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include "Controller.h"
 
-
-const int WIDTH = 1000; //to send the parameters
-const int HEIGHT = 600;
-
+void setWindowDimentions(float& width, float& height);
 
 int main()
 {
-	Controller game(WIDTH,HEIGHT);
+	float width, height;
+
+	setWindowDimentions(width, height);
+	Controller game(width,height);
 	game.operateGame();
 	
 	return 0;
+}
+
+void setWindowDimentions(float& width, float& height)
+{
+	// temp sf::Texture to optain the dimentions of the windows
+	sf::Texture tempTex;
+	tempTex.loadFromFile("Background.png");
+
+	// width and height of window = width and height of background image
+	width = tempTex.getSize().x;
+	height = tempTex.getSize().y;
 }
